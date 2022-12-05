8fdbb582243865e31ba3ba49a880c29973fa8ad8
G-0324D <byesad@gmail.com>
2022-12-05 04:44:03 +0300

    Update - Table
    ProductList.vue içerisinde ürünlerimizi listelemek için Bootstrap stilinde bir tablo oluşturduk ve ürünleri çağırdık.

--- project/src/components/ProductList.vue
+++ project/src/components/ProductList.vue
@@ -3,7 +3,29 @@
 
         <h3> Product List</h3>
 
-        {{products.length}}
+        <p v-if="products.length==0">Stok'ta ürün bulunmamaktadır.</p>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>ürün Adı</th>
+                    <th>Kategori</th>
+                    <th>Ürün Açıklama</th>
+                    <th>Birim Fiyat</th>
+                    <th>Stok</th>
+                </tr>
+            </thead>
+            <tbody>
+                    <tr v-for="product in products" :key="product.id">
+                        <td>{{product.id}}</td>
+                        <td>{{product.productName}}</td>
+                        <td>{{product.categoryId}}</td>
+                        <td>{{product.quantityPerUnit}}</td>
+                        <td>{{product.unitPrice}}</td>
+                        <td>{{product.unitInStock}}</td>
+                    </tr>
+                </tbody>
+        </table>
 
     </div>
 </template>
