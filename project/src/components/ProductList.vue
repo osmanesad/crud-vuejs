7840fccd417eb24686b0eb30cddf48902c6b8c77
G-0324D <byesad@gmail.com>
2022-12-05 18:10:08 +0300

    Update - Update Option
    Güncelleme işleminin ilk aşamasını gerçekleştirdik. Her ürün için düzenleme modunu aktif ettik.

--- project/src/components/ProductList.vue
+++ project/src/components/ProductList.vue
@@ -18,13 +18,48 @@
             </thead>
             <tbody>
                     <tr v-for="product in products" :key="product.id">
-                        <td>{{product.id}}</td>
-                        <td>{{product.productName}}</td>
-                        <td>{{product.categoryId}}</td>
-                        <td>{{product.quantityPerUnit}}</td>
-                        <td>{{product.unitPrice}}</td>
-                        <td>{{product.unitInStock}}</td>
-                        <td><button class="btn btn-sm btn-danger" @click="handleDelete(product)">Sil</button></td>
+                        
+                        <td v-if="(updateId === product.id)">
+                            <input v-model="product.id" type="text" class="form-control" id="id"/>
+                        </td>
+                        <td v-else>{{product.id}}</td>
+
+                        <td v-if="(updateId === product.id)">
+                            <input v-model="product.productName" type="text" class="form-control" id="productName"/>
+                        </td>
+                        <td v-else>{{product.productName}}</td>
+
+                        <td v-if="(updateId === product.id)">
+                            <input v-model="product.categoryId" type="text" class="form-control" id="categoryId"/>
+                        </td>
+                        <td v-else>{{product.categoryId}}</td>
+
+                        <td v-if="(updateId === product.id)">
+                            <input v-model="product.quantityPerUnit" type="text" class="form-control" id="quantityPerUnit"/>
+                        </td>
+                        <td v-else>{{product.quantityPerUnit}}</td>
+
+                        <td v-if="(updateId === product.id)">
+                            <input v-model="product.unitPrice" type="text" class="form-control" id="unitPrice"/>
+                        </td>
+                        <td v-else>{{product.unitPrice}}</td>
+
+                        <td v-if="(updateId === product.id)">
+                            <input v-model="product.unitInStock" type="text" class="form-control" id="unitInStock"/>
+                        </td>
+                        <td v-else>{{product.unitInStock}}</td>
+
+                       
+                        <td v-if="updateId!==product.id">
+                            <button class="btn btn-sm btn-primary" @click="handleUpdate(product)">Güncelle</button>
+                            <button class="btn btn-sm btn-danger" @click="handleDelete(product)">Sil</button>
+                        </td>
+                        <td v-else>
+                            <button class="btn btn-sm btn-primary" @click="handleUpdate(product)">Kaydet</button>
+                            <button class="btn btn-sm btn-danger" @click="handleDelete(product)">İptal</button>
+                        </td>
+
+
                     </tr>
                 </tbody>
         </table>
@@ -36,7 +71,10 @@
 
 <script>
 export default {
-    name:"product-list",
+    name: "product-list",
+    data() {
+        return { updateId: null };
+    },
     props:{
         products:Array
     },
@@ -45,8 +83,8 @@ export default {
             this.$emit("delete:product",product)
             
         },
-        handleUpdate() {
-            
+        handleUpdate(product) {
+            this.updateId = product.id
         }
     }
 };
