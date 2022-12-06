66ef76dd13408b22c6570f244ef935eaead57e0a
G-0324D <byesad@gmail.com>
2022-12-06 04:02:25 +0300

    Update - ProductAdd

--- project/src/components/ProductAdd.vue
+++ project/src/components/ProductAdd.vue
@@ -1,62 +1,98 @@
 <template>
   <div id="addForm">
     <form @submit.prevent="handleAdd">
+      <div class="form-group">
+        <label for="id">Id</label>
+        <input
+          v-model="product.id"
+          placeholder="ID Otomatik atanmaktadır."
+          readonly
+          type="text"
+          class="form-control"
+          id="id"
+        />
+      </div>
 
-        <div class="form-group">
-            <label for="id">Id</label>
-            <input v-model="product.id" placeholder="ID Otomatik atanmaktadır." readonly type="text" class="form-control" id="id" />
-        </div>
+      <div class="form-group">
+        <label for="categoryName">Kategori</label>
+        <input
+          v-model="product.categoryName"
+          type="text"
+          class="form-control"
+          id="categoryName"
+        />
 
-        <div class="form-group">
-            <label for="categoryId">Kategori</label>
-            <input v-model="product.categoryId" type="text" class="form-control" id="categoryId" />
-        </div>
+        
 
-        <div class="form-group">
-            <label for="productName">Ürün Adı</label>
-            <input v-model="product.productName" type="text" class="form-control" id="productName" />
-        </div>
+      </div>
 
-        <div class="form-group">
-            <label for="quantityPerUnit">Açıklama</label>
-            <input v-model="product.quantityPerUnit" type="text" class="form-control" id="quantityPerUnit" />
-        </div>
+      <div class="form-group">
+        <label for="productName">Ürün Adı</label>
+        <input
+          v-model="product.productName"
+          type="text"
+          class="form-control"
+          id="productName"
+        />
+      </div>
 
-        <div class="form-group">
-            <label for="unitPrice">Birim Fiyat</label>
-            <input v-model="product.unitPrice" type="text" class="form-control" id="unitPrice" />
-        </div>
+      <div class="form-group">
+        <label for="quantityPerUnit">Açıklama</label>
+        <input
+          v-model="product.quantityPerUnit"
+          type="text"
+          class="form-control"
+          id="quantityPerUnit"
+        />
+      </div>
 
-        <div class="form-group">
-            <label for="v">Stok</label>
-            <input v-model="product.unitInStock" type="text" class="form-control" id="unitInStock" />
-        </div>
-        <br/>
+      <div class="form-group">
+        <label for="unitPrice">Birim Fiyat</label>
+        <input
+          v-model="product.unitPrice"
+          type="text"
+          class="form-control"
+          id="unitPrice"
+        />
+      </div>
 
-        <button type="submit" class="btn btn-success">Ekle</button>
+      <div class="form-group">
+        <label for="v">Stok</label>
+        <input
+          v-model="product.unitInStock"
+          type="text"
+          class="form-control"
+          id="unitInStock"
+        />
+      </div>
+      <br />
 
+      <button type="submit" class="btn btn-success">Ekle</button>
     </form>
   </div>
 </template>
 
 <script>
-
-
 export default {
-    name: "product-add",
-    data() {
-        return {
-            product:{id:'',categoryId:'',productName:'',quantityPerUnit:'',unitPrice:'',unitInStock:''}
-        }
+  name: "product-add",
+  data() {
+    return {
+      product: {
+        id: "",
+        categoryId: "",
+        productName: "",
+        quantityPerUnit: "",
+        unitPrice: "",
+        unitInStock: "",
+      },
+    };
+  },
+  methods: {
+    handleAdd() {
+      this.$emit("add:product", this.product);
     },
-    methods: {
-        handleAdd() {
-            this.$emit("add:product", this.product)
-        }
-    }
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
